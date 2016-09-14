05630b03d6caddcb5cd1b94458bbfd626e14679c
Taylor Page <jonathantaylorpage@gmail.com>
2016-09-14 10:59:24 -0500

    (chore) Create middleware

---
+++ server/middleware/middleware.js
@@ -0,0 +1,8 @@
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+module.exports = function (app, express) {
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+};
