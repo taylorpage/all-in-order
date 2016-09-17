f0f61ba7c38f347939f9c04ceb662fe1c11bd042
Taylor Page <jonathantaylorpage@gmail.com>
2016-09-17 11:58:50 -0500

    (test) Add all passing customer test expectations

--- spec/customerSpec.js
+++ spec/customerSpec.js
@@ -15,20 +15,49 @@ describe('Routing', function() {
         permalink: 'perma.link3'
       }
 
-      request.post(`${url}/customer/create`, { form: req }, (data) => {
+      request.post(`${url}/customer/create`, { form: req }, (err, data, body) => {
+
+        body = JSON.parse(body);
+
+        expect(body.name).to.equal('Jane Doe');
+        expect(body.items).to.be.an('array');
+        done();
+      })
+    });
+
+    it('should create a second customer', function(done) {
+      let req = {
+        name: 'John Doe',
+        state: 'Testing',
+        property: 'Address',
+        permalink: 'perma.link4'
+      }
+
+      request.post(`${url}/customer/create`, { form: req }, (err, data, body) => {
+
+        body = JSON.parse(body);
+
+        expect(body.name).to.equal('John Doe');
+        expect(body.permalink).to.equal('perma.link4');
         done();
       })
     });
   });
 
   describe('customer/add/item', function() {
-    it('should return all customer\'s data', function(done) {
+    it('should add an item to customer list', function(done) {
       let req = {
         permalink: 'perma.link3',
         item: { name: 'test', status: 'test' }
       }
 
-      request.post(`${url}/customer/add/item`, { form: req }, (data) => {
+      request.post(`${url}/customer/add/item`, { form: req }, (err, data, body) => {
+
+        body = JSON.parse(body);
+
+        expect(body.ok).to.equal(1);
+        expect(body.nModified).to.equal(1);
+        expect(body.n).to.equal(1);
         done();
       })
     });
@@ -36,12 +65,13 @@ describe('Routing', function() {
 
   describe('customer/all', function() {
     it('should return all customer\'s data', function(done) {
-      request(`${url}/customer/all`, (data) => {
-        done();
-      })
-    });
-    it('should contain proper data', function(done) {
-      request(`${url}/customer/all`, (data) => {
+      request(`${url}/customer/all`, (err, data, body) => {
+
+        body = JSON.parse(body);
+
+        expect(body).to.have.length(2);
+        expect(body[0]).to.have.property('permalink');
+        expect(body[1]).to.have.property('permalink');
         done();
       })
     });
@@ -53,7 +83,13 @@ describe('Routing', function() {
         permalink: 'perma.link3'
       };
 
-      request.post(`${url}/customer/items`, { form: req }, (data) => {
+      request.post(`${url}/customer/items`, { form: req }, (err, data, body) => {
+
+        body = JSON.parse(body);
+
+        expect(body).to.have.length(1);
+        expect(body[0]).to.have.property('name');
+        expect(body[0]).to.have.property('status');
         done();
       })
     });
@@ -63,11 +99,17 @@ describe('Routing', function() {
     it('should return all customer\'s data', function(done) {
       const req = {
         permalink: 'perma.link3',
-        item: { name: 'test', status: 'change' }
+        item: { name: 'test', status: 'changed' }
       }
 
-      request.post(`${url}/customer/status`, { form: req }, (data) => {
-        done();
+      request.post(`${url}/customer/status`, { form: req }, (err, data, body) => {
+        request.post(`${url}/customer/items`, { form: req }, (err, data, body) => {
+
+          body = JSON.parse(body);
+
+          expect(body[0].status).to.equal('changed')
+          done();
+        });
       })
     });
   });
